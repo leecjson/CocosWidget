c1e253a2eca0ba71cafdcdedafacf7ca879d4644
csdn-viva <csdn_viva@foxmail.com>
2013-12-13 15:16:11 +0800

    clear all for the new space

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /cygdrive/f/cocos/cocos2d-x-2.2.0/projects/CCWidgetTest/proj.android/../../../cocos2dx/ccFPSImages.c \
- /cygdrive/f/cocos/cocos2d-x-2.2.0/projects/CCWidgetTest/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/cygdrive/f/cocos/cocos2d-x-2.2.0/projects/CCWidgetTest/proj.android/../../../cocos2dx/ccFPSImages.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /cygdrive/f/cocos/cocos2d-x-2.2.0/projects/CCWidgetTest/proj.android/../../../cocos2dx/support/ccUtils.cpp \
- /cygdrive/f/cocos/cocos2d-x-2.2.0/projects/CCWidgetTest/proj.android/../../../cocos2dx/support/ccUtils.h
-
-/cygdrive/f/cocos/cocos2d-x-2.2.0/projects/CCWidgetTest/proj.android/../../../cocos2dx/support/ccUtils.h:
