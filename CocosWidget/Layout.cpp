d115692dfc2f03bcd3cbf9e87f83e033d2eea2fd
Viva-Lijunlin <csdn_viva@foxmail.com>
2014-01-12 03:49:55 +0800

    change user name

--- CocosWidget/Layout.cpp
+++ CocosWidget/Layout.cpp
@@ -173,10 +173,7 @@ CCObject* CLayout::find(CCArray* pChidren, const char* id)
 				else
 				{
 					CCObject* pRet = find(dynamic_cast<CCNode*>(pObject)->getChildren(), id);
-					if( pRet )
-					{
-						return pRet;
-					}
+					if( pRet ) return pRet;
 				}
 			}
 		}
